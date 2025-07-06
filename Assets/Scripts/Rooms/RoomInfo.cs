81717385f5919fe2ee51c95afdd7a15d5a2f0e42
KOSMOGOR <mortalmortal2015@mail.ru>
2025-07-06 23:30:48 +0300

    update: changed generation for rooms to match

--- Assets/Scripts/Rooms/RoomInfo.cs
+++ Assets/Scripts/Rooms/RoomInfo.cs
@@ -5,10 +5,45 @@ using UnityEngine;
 public class RoomInfo : ScriptableObject
 {
     public string sceneName;
-    public bool up;
-    public bool down;
-    public bool left;
-    public bool right;
+    public RoomDirections rd;
+}
+
+[Serializable]
+public class RoomDirections {
+    public DirAvailability up;
+    public DirAvailability down;
+    public DirAvailability left;
+    public DirAvailability right;
+
+    public static RoomDirections Any() {
+        return new RoomDirections{
+            up = DirAvailability.Any,
+            down = DirAvailability.Any,
+            left = DirAvailability.Any,
+            right = DirAvailability.Any
+        };
+    }
+
+    public bool CanFit(RoomDirections other) {
+        bool r = up == DirAvailability.Any || up == other.up;
+        r &= down == DirAvailability.Any || down == other.down;
+        r &= left == DirAvailability.Any || left == other.left;
+        r &= right == DirAvailability.Any || right == other.right;
+        return r;
+    }
+
+    public override string ToString() {
+        return $"{(up == DirAvailability.Available ? "U" : up == DirAvailability.Any ? "u" : "")}" + 
+            $"{(down == DirAvailability.Available ? "D" : down == DirAvailability.Any ? "d" : "")}" + 
+            $"{(left == DirAvailability.Available ? "L" : left == DirAvailability.Any ? "l" : "")}" + 
+            $"{(right == DirAvailability.Available ? "R" : right == DirAvailability.Any ? "r" : "")}";
+    }
+}
+
+public enum DirAvailability {
+    Available,
+    NotAvailable,
+    Any
 }
 
 public class RoomInfoInstance
