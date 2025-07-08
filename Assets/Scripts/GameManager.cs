cd3a8595da0c6132a985653155a32d51a31122fd
Cdeth567 <11kvvkvv11@mail.ru>
2025-07-08 05:54:52 +0300

    death screen added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,10 +5,13 @@ public class GameManager : MonoBehaviour
 {
     private Player player;
     [SerializeField] private float respawnDelay = 1f;
+    [SerializeField] private string mainMenuSceneName = "MainMenu";
+    [SerializeField] private string levelCave000 = "LevelCave000";
 
     public static GameManager I;
 
-    void Awake() {
+    void Awake()
+    {
         if (I != null) return;
         I = this;
     }
@@ -18,13 +21,13 @@ public class GameManager : MonoBehaviour
         player = GameObject.Find("mage").GetComponent<Player>();
     }
 
-    void Update()
-    {
-        if (player.GetHP() <= 0f)
-        {
-            Invoke(nameof(RespawnPlayer), respawnDelay);
-        }
-    }
+    // void Update()
+    // {
+    //     if (player.GetHP() <= 0f)
+    //     {
+    //         Invoke(nameof(RespawnPlayer), respawnDelay);
+    //     }
+    // }
 
     public void PlayerDied()
     {
@@ -34,5 +37,19 @@ public class GameManager : MonoBehaviour
     private void RespawnPlayer()
     {
         SceneManager.LoadScene("LevelCave000");
+    }
+
+    public void RestartGame()
+    {
+        Debug.Log("Restart Clicked");
+        SceneManager.LoadScene(levelCave000);
+        Time.timeScale = 1f;
+    }
+
+    public void ToMainMenu()
+    {
+        Debug.Log("Main Menu Clicked");
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(mainMenuSceneName);
     }
 }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
@@ -8,9 +9,40 @@ public class Player : MonoBehaviour
     [SerializeField] private int hp;
     [SerializeField] private int maxHP = 10;
     [SerializeField] float iSecondsCount = 2;
+    [SerializeField] private GameObject deathScreenPrefab;
+    private GameObject deathScreenInstance;
+    private Player Instance;
+    private bool isDead = false;
+
+
+    void Awake()
+    {
+        Instance = this;
+
+        if (deathScreenPrefab != null)
+        {
+            deathScreenInstance = Instantiate(deathScreenPrefab);
+            deathScreenInstance.SetActive(false);
+
+            var buttons = deathScreenInstance.GetComponentsInChildren<Button>();
+            if (buttons.Length >= 2)
+            {
+                buttons[0].onClick.RemoveAllListeners();
+                buttons[1].onClick.RemoveAllListeners();
+
+                buttons[0].onClick.AddListener(GameManager.I.RestartGame);
+                buttons[1].onClick.AddListener(GameManager.I.ToMainMenu);
+            }
+        }
+        else
+        {
+            Debug.Log("DeathScreenPrefab не найден");
+        }
+    }
 
     void Start()
     {
+        isDead = false;
         hp = maxHP;
     }
 
@@ -23,8 +55,12 @@ public class Player : MonoBehaviour
             hp--;
             Debug.Log($"Player HP: {hp}");
         }
+        // for test
+        if (hp <= 0 && !isDead)
+        {
+            Die();
+        }
     }
-    // for test
 
     public int GetHP()
     {
@@ -43,11 +79,33 @@ public class Player : MonoBehaviour
         hp = maxHP;
     }
 
-    public void TakeDamage(int damage) {
+    public void TakeDamage(int damage)
+    {
         if (hp <= 0 || iSecondsCount > 0) return;
         hp = Mathf.Max(hp - damage, 0);
         iSecondsCount = iSeconds;
         Debug.Log($"HP {hp}");
-        if (hp <= 0) GameManager.I.PlayerDied();
+        if (hp <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        isDead = true;
+        // GameManager.I.PlayerDied();
+        
+        Debug.Log("Die");
+        Time.timeScale = 0;
+
+        var characterControl = GetComponent<CharacterController>();
+        if (characterControl != null)
+            characterControl.enabled = false;
+
+        if (deathScreenInstance != null)
+        {
+            deathScreenInstance.SetActive(true);
+        }
     }
 }
