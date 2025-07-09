082b923b7ff8dce378a760178a97710e6018d64b
Cdeth567 <11kvvkvv11@mail.ru>
2025-07-09 20:53:54 +0300

    delete uding statement

--- Assets/Scripts/Player/movement.cs
+++ Assets/Scripts/Player/movement.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 [RequireComponent(typeof(Rigidbody2D), typeof(Animator), typeof(SpriteRenderer))]
 public class PlayerController : MonoBehaviour
