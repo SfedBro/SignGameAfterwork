76fa4e232a9f2158cbc80243ccc24c9c317a2318
KOSMOGOR <mortalmortal2015@mail.ru>
2025-07-17 16:57:26 +0300

    add: boss laser attack

---
+++ Assets/Prefabs/enemies/Boss/Phase1/Attacks/BossLaser.cs
@@ -0,0 +1,30 @@
+using System.Linq;
+using UnityEngine;
+
+public class BossLaser : MonoBehaviour
+{
+    public float laserMaxDistance = 10;
+    public int damage = 1;
+
+    LineRenderer lineRenderer;
+
+    void Awake() {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    void Update() {
+        ShootLaser();
+    }
+
+    void ShootLaser() {
+        RaycastHit2D circleHit = Physics2D.CircleCast(transform.position, lineRenderer.startWidth / 2, transform.right, laserMaxDistance);
+        if (circleHit) {
+            DrawLaser(transform.position, transform.position + transform.right * Vector2.Distance(transform.position, circleHit.point));
+            if (circleHit.transform.gameObject.CompareTag("Player")) circleHit.transform.GetComponent<Player>().TakeDamage(damage);
+        } else DrawLaser(transform.position, transform.position + transform.right * laserMaxDistance);
+    }
+
+    void DrawLaser(Vector3 startPos, Vector3 endPos) {
+        lineRenderer.SetPositions(new[]{startPos, endPos});
+    }
+}
