cae0192cdf0671870036bd6c371676346c8c2605
SfedBro <fegalinka@mail.ru>
2025-07-19 19:50:38 +0300

    Slash attack made, not integrated

--- Assets/Prefabs/enemies/Boss/Phase2/Attacks/SingleSlashScript.cs
+++ Assets/Prefabs/enemies/Boss/Phase2/Attacks/SingleSlashScript.cs
@@ -13,6 +13,8 @@ public class SingleSlashScript : MonoBehaviour
     private float growMultiplier;
     [SerializeField]
     private Vector2 flyingDirection;
+    [SerializeField]
+    private string neededElement;
     private void Start()
     {
         StartCoroutine(DestroyAfterTime(lifeTime));
@@ -27,15 +29,49 @@ public class SingleSlashScript : MonoBehaviour
     }
     private void MoveSlash()
     {
-        transform.Translate(flyingDirection.normalized * speed * Time.deltaTime, Space.World);
+        transform.Translate(flyingDirection.normalized * speed * Time.deltaTime, Space.Self);
     }
     private void ChangeSize()
     {
-        transform.localScale *= (1f + growMultiplier * Time.deltaTime);
+        transform.localScale += new Vector3(1, 1) * growMultiplier * Time.deltaTime;
     }
     private IEnumerator DestroyAfterTime(float time)
     {
         yield return new WaitForSeconds(time);
         Destroy(gameObject);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<SpellCast>())
+        {
+            Debug.Log("Collision");
+            string element = collision.gameObject.GetComponent<SpellCast>().LastSpellUsed().MainElement;
+            if (element != neededElement)
+            {
+                if (collision.gameObject.GetComponent<Player>())
+                {
+                    collision.gameObject.GetComponent<Player>().TakeDamage(damage);
+                }
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<SpellCast>())
+        {
+            bool shouldTakeDamage = true;
+            if (collision.gameObject.GetComponent<SpellCast>().LastSpellUsed() != null)
+            {
+                string element = collision.gameObject.GetComponent<SpellCast>().LastSpellUsed().MainElement;
+                if (element == neededElement)
+                {
+                    shouldTakeDamage = false;
+                }
+            }
+            if (shouldTakeDamage && collision.gameObject.GetComponent<Player>())
+            {
+                collision.gameObject.GetComponent<Player>().TakeDamage(damage);
+            }
+        }
     }
 }
