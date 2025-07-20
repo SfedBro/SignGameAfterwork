863a076ac76815c4fb3a101af3542b0f59a5b452
SfedBro <fegalinka@mail.ru>
2025-07-20 17:01:32 +0300

    added top door to boss room

--- Assets/Prefabs/enemies/Boss/DoorClosing.cs
+++ Assets/Prefabs/enemies/Boss/DoorClosing.cs
@@ -3,29 +3,34 @@ using UnityEngine;
 public class DoorClosing : MonoBehaviour
 {
     [SerializeField]
-    private GameObject door;
+    private GameObject doorL;
+    [SerializeField]
+    private GameObject doorU;
     private bool playerInside = false;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             playerInside = true;
-            door.SetActive(true);
+            doorL.SetActive(true);
+            doorU.SetActive(true);
         }
     }
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.CompareTag("Player") && door.activeSelf)
+        if (other.CompareTag("Player") && (doorL.activeSelf || doorU.activeSelf))
         {
             playerInside = false;
-            door.SetActive(false);
+            doorL.SetActive(false);
+            doorU.SetActive(false);
         }
     }
     private void Update()
     {
-        if (!playerInside && door.activeSelf)
+        if (!playerInside && (doorL.activeSelf || doorU.activeSelf))
         {
-            door.SetActive(false);
+            doorL.SetActive(false);
+            doorU.SetActive(false);
         }
     }
 }
