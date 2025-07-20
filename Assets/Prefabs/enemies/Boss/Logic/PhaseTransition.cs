e70875cfede19ef5fabea0651beaf672fdd5dd13
SfedBro <fegalinka@mail.ru>
2025-07-20 19:10:20 +0300

    productive work with KOSMOGOR

--- Assets/Prefabs/enemies/Boss/Logic/PhaseTransition.cs
+++ Assets/Prefabs/enemies/Boss/Logic/PhaseTransition.cs
@@ -2,13 +2,18 @@ using UnityEngine;
 
 public class PhaseTransition : MonoBehaviour
 {
-    public Phase1 phase1;
+    public Phases phases;
     public Collider2D col1;
-    public Phase2 phase2;
     public Collider2D col2;
     public ParticleSystem ps;
 
     public void StartBattle() {
-        phase1.StartPhase();
+        phases.isPhaseInProcess = true;
+        phases.StartPhaseOne();
+    }
+    public void ChangePhase()
+    {
+        phases.isPhaseInProcess = false;
+        phases.StartPhaseTwo();
     }
 }
--- Assets/Prefabs/enemies/Boss/Logic/Phases.cs
+++ Assets/Prefabs/enemies/Boss/Logic/Phases.cs
@@ -5,30 +5,40 @@ using System.Linq;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class Phase1 : MonoBehaviour
+public class Phases : MonoBehaviour
 {
     public List<AttackInfo> attackPrefAndWarn;
     public float attackTimeout;
     public List<GameObject> enemyPrefabs;
 
+    public List<AttackInfo> attackPrefAndWarn2;
+    public float attackTimeout2;
+    public List<GameObject> enemyPrefabs2;
+
     int lastAttackIndex = -1;
     public Coroutine curPhaseCoroutine;
+    public bool isPhaseInProcess;
 
     void Start() {
         // StartPhase();
     }
 
-    public void StartPhase() {
+    public void StartPhaseOne() {
         curPhaseCoroutine = StartCoroutine(PhaseCoroutine());
+    }
+    public void StartPhaseTwo()
+    {
+        curPhaseCoroutine = StartCoroutine(WaitForEndOfThePhase());
     }
 
     IEnumerator PhaseCoroutine() {
-        while (true) {
+        while (isPhaseInProcess) {
             AttackInfo attack = attackPrefAndWarn.Where((at, i) => i != lastAttackIndex).OrderBy(at => Random.value).First();
             lastAttackIndex = attackPrefAndWarn.IndexOf(attack);
             StartCoroutine(PerformAttack(attack));
             yield return new WaitForSeconds(attackTimeout);
         }
+        curPhaseCoroutine = null;
     }
 
     IEnumerator PerformAttack(AttackInfo attack) {
@@ -38,6 +48,31 @@ public class Phase1 : MonoBehaviour
         GameObject curAttack = Instantiate(attack.attackPrefab, transform, false);
         yield return new WaitForSeconds(attack.attackTime);
         Destroy(curAttack);
+    }
+    IEnumerator WaitForEndOfThePhase()
+    {
+        while (isPhaseInProcess || curPhaseCoroutine != null)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+        curPhaseCoroutine = StartCoroutine(PhaseTwoCoroutine());
+        isPhaseInProcess = true;
+    }
+    IEnumerator PhaseTwoCoroutine()
+    {
+        while (isPhaseInProcess)
+        {
+            AttackInfo attack = attackPrefAndWarn2.Where((at, i) => i != lastAttackIndex).OrderBy(at => Random.value).First();
+            lastAttackIndex = attackPrefAndWarn2.IndexOf(attack);
+            StartCoroutine(PerformAttack(attack));
+            yield return new WaitForSeconds(attackTimeout2);
+        }
+        curPhaseCoroutine = null;
+    }
+    IEnumerator TimeoutAfterAttack(float time)
+    {
+        transform.position = new Vector3(0, 0, 0);
+        yield return new WaitForSeconds(time);
     }
 }
 
